32ab5d1e54b01a3d3b05372728b337cb146eae0f
Gaitan19 <kenleyjos619@gmail.com>
2023-05-27 22:14:26 -0600

    Script de la BD

---
+++ DB_tareas.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE[Gtareas]
+GO
+USE[Gtareas]
+GO
+
+--Tabla de las tareas
+CREATE TABLE Tareas(
+idTarea int primary key identity(1,1),
+titulo varchar(100),
+estado bit default 1,  --1 es para incompleta y 0 para completada
+estadoEli bit default 1  --0 es eliminado
+)
+Go
