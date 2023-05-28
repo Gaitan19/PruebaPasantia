126a65db7f21ddc7fdff56dcbf081cf4d0eabb7c
Gaitan19 <kenleyjos619@gmail.com>
2023-05-28 09:01:04 -0600

    TareasAPI

---
+++ TareasAPI/Data/GtareasContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TareasAPI.Data.TareaModels;
+
+namespace TareasAPI.Data;
+
+public partial class GtareasContext : DbContext
+{
+    public GtareasContext()
+    {
+    }
+
+    public GtareasContext(DbContextOptions<GtareasContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.HasKey(e => e.IdTarea).HasName("PK__Tareas__756A540249EA8598");
+
+            entity.Property(e => e.IdTarea).HasColumnName("idTarea");
+            entity.Property(e => e.Estado)
+                .HasDefaultValueSql("((1))")
+                .HasColumnName("estado");
+            entity.Property(e => e.EstadoEli)
+                .HasDefaultValueSql("((1))")
+                .HasColumnName("estadoEli");
+            entity.Property(e => e.Titulo)
+                .HasMaxLength(100)
+                .IsUnicode(false)
+                .HasColumnName("titulo");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ TareasAPI/Data/TareaModels/Tarea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TareasAPI.Data.TareaModels;
+
+public partial class Tarea
+{
+    public int IdTarea { get; set; }
+
+    public string? Titulo { get; set; }
+
+    public bool? Estado { get; set; }
+
+    public bool? EstadoEli { get; set; }
+}
