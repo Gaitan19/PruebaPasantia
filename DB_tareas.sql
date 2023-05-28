3228e6117f89df4241a72a9263dd9d4c25c756c8
Gaitan19 <kenleyjos619@gmail.com>
2023-05-28 17:58:15 -0600

    scrip de la BD

--- DB_tareas.sql
+++ DB_tareas.sql
@@ -7,8 +7,9 @@ GO
 CREATE TABLE Tareas(
 idTarea int primary key identity(1,1),
 titulo varchar(100),
-estado bit default 1,  --1 es para incompleta y 0 para completada
+estado_no_Completado bit default 1,  --1 es para incompleta y 0 para completada
 estadoEli bit default 1  --0 es eliminado
 )
 Go
 
+select * from Tareas
